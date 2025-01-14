fe727fe8f2eaec16ba398da5559238bc84ea6fe6
Mayorgamin <alexismayorga@gmail.com>
2025-01-14 22:36:48 +0000

    Add functionality to calculate sum, average, and largest number from a list of user inputs

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,47 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        int newNumber = -1;
+        
+        while (newNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            newNumber = int.Parse(Console.ReadLine());
+
+           
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber); 
+      
+            }         
+            
+        }
+
+        int sum = 0;
+        int largest = int.MinValue;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > largest)
+            {
+                largest = number;
+            }
+
+
+        } 
+            double average = (double)sum / numbers.Count;
+            
+    
+            Console.WriteLine($"The sum is: {sum} ");
+            Console.WriteLine($"The average is: {average} ");
+            Console.WriteLine($"The largest number is: {largest}");
+            Console.WriteLine();
     }
 }
