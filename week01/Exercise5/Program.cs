a871be625ce1eef7b85080ea4ca2a143c7b6dc11
Mayorgamin <alexismayorga@gmail.com>
2025-01-15 00:38:00 +0000

    Implement program with functions for square calculation

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,54 @@ using System;
 
 class Program
 {
+
+    static string name;
+    static int number;
+    static int square;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();
+        SquareNumber();
+        DisplayResult();
+
+    }
+
+
+
+    static void DisplayWelcome()
+    {   
+        Console.WriteLine("Welcome to the program!");
+    }
+
+       
+        static void PromptUserName()
+    {   
+        Console.Write("Please enter your name: ");
+        name = Console.ReadLine();
+    }
+
+        static void PromptUserNumber()
+     {   
+         Console.Write("Please enter your favorite number: ");
+         number = int.Parse(Console.ReadLine());
+    
+     }
+    
+        static void SquareNumber()
+    {   
+
+        square = number * number;
+
+    }
+
+
+        static void DisplayResult()
+    {   
+        Console.Write($"Brother {name}, the square of your number is {square}");
+        Console.WriteLine();
+
     }
 }
