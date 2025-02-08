15eb12e53c3c33a6e38e288a90eaea4588b7ac66
Mayorgamin <alexismayorga@gmail.com>
2025-02-08 00:02:54 +0000

    Completed homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment myAssignment = new Assignment("Francisco Mayorga", "Chemistry");
+        string summary = myAssignment.GetSummary();
+        Console.WriteLine("Assignment Summary:");
+        Console.WriteLine(summary);
+
+        MathAssignment myMathAssignment = new MathAssignment("Carolina Astorga", "Geometry", "6.9", "2 * 5");
+        string summaryMath = myMathAssignment.GetHomeworkList();
+        Console.WriteLine("\nMath Assignment:");
+        Console.WriteLine(summaryMath);
+
+        WritingAssignment myWritingAssignment = new WritingAssignment("Owain Hayles", "Spanish", "La Consagración");
+        string summaryWriting = myWritingAssignment.GetWritingInformation();
+        Console.WriteLine("\nWriting Assignment:");
+        Console.WriteLine(summaryWriting);
+
     }
 }
+
