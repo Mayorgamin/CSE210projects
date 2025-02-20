19b328aa511b683c3753d5a99c5e46b3f393de9f
Mayorgamin <alexismayorga@gmail.com>
2025-02-20 21:41:20 +0000

    implemented shape base class and derived classes with polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add shapes to the list
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Iterate through the list and display
+        foreach (Shape shape in shapes)
+        {
+            Console. WriteLine($"Shape color: {shape.GetColor()}");
+            Console.WriteLine($"Shape area: {shape.GetArea()}");
+        }
+
+
+
+
+
+
+
     }
 }
